9c69a07bb248756fb358dc6a83f13a4dd06fe83a
Dlmitriyyy <streltsowdm@gmail.com>
2023-10-19 16:09:57 +0300

    Добавьте файлы проекта.

---
+++ 01010536.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	system("chcp 1251 > nul");
+	int days;
+	double distance = 1.5;
+	cout << "Введіть кількість днів в які черепаха рухалась:" << endl;
+	cin >> days;
+	if (days < 0)
+	{
+		cout << "Кількість днів не може бути менше нуля.";
+		return 0;
+	}
+	for (days >= 0; days--; distance += 0.20);
+	{
+		cout << distance << "м.";
+	}
+}
